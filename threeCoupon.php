36e5bae5604c977c575d278f82a9af9153d7fa03
r2d2c3po <r2d2c3po.ai@gmail.com>
2014-05-26 02:01:27 -0400

    version one

---
+++ threeCoupon.php
@@ -0,0 +1,28 @@
+<head>
+<link rel="stylesheet" type="text/css" href="coupon.css">
+</head>
+
+<div class="container">
+<div class="content">
+<div class="header">Hong Kong Restaurant
+</div>
+
+<div class="address"> 
+1345 W. Southport Rd. Indianapolis, IN 46217 <br>
+<img id="logo" src="logo.png">
+<div id="contact">
+317-887-6918 <br>
+8876918.com <br>
+</div>
+</div>
+
+<img src="3dollars.png">
+<center>$3 off for any order over $25!</center><br>
+<span style="font-size:10px">
+Print or show on phone for redemption, limit one coupon per visit.
+<?php
+echo "Time visited:". date("F j, Y, h:i a",time());
+?></span>
+
+</div>
+</div>
---
+++ twoCoupon.php
@@ -0,0 +1,28 @@
+<head>
+<link rel="stylesheet" type="text/css" href="coupon.css">
+</head>
+
+<div class="container">
+<div class="content">
+<div class="header">Hong Kong Restaurant
+</div>
+
+<div class="address"> 
+1345 W. Southport Rd. Indianapolis, IN 46217 <br>
+<img id="logo" src="logo.png">
+<div id="contact">
+317-887-6918 <br>
+8876918.com<br>
+</div>
+</div>
+
+<img src="2dollars.png">
+<center>$2 off for any order over $18!</center><br>
+<span style="font-size:10px">
+Print or show on phone for redemption, limit one coupon per visit.
+<?php
+echo "Time visited:". date("F j, Y, h:i a",time());
+?></span>
+
+</div>
+</div>
